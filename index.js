68f9522da7a30361d31655bc1feb3f804dd025c5
Craig Ford <craig_ford@icloud.com>
2022-01-31 13:14:13 -0500

    commit tree 5d847df055b4d0dff7230f8a794aff1e0d925378

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = welcomeMessage.slice(0,7) + ", " + firstInitial + "!";
